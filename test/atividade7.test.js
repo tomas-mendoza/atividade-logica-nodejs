c957fa918c62c95ac1b5cba16af87cef35986ac6
leandro-sebold <105085056+leandro-sebold@users.noreply.github.com>
2024-02-24 20:14:33 -0300

    Update atividade7.test.js

--- test/atividade7.test.js
+++ test/atividade7.test.js
@@ -4,8 +4,8 @@ test('Calcular a média ponderada de 3 notas: 5, 5, 5, expected: 5', () => {
   expect(main(5, 5, 5)).toBe(5);
 });
 
-test('Calcular a média ponderada de 3 notas: 8, 7, 6, expected: 6.833333333333333', () => {
-  expect(main(8, 7, 6)).toBe(6.833333333333333);
+test('Calcular a média ponderada de 3 notas: 10, 10, 10, expected: 10', () => {
+  expect(main(10, 10, 10)).toBe(10);
 });
 
 test('Calcular a média ponderada de 3 notas: 0, 0, 0, expected: 0', () => {
