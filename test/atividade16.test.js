6ae2f4f3209581ba28ad563ee5b4014e753c24a3
leandro-sebold <105085056+leandro-sebold@users.noreply.github.com>
2024-02-24 21:55:04 -0300

    Atualiza e corrige testes

--- test/atividade16.test.js
+++ test/atividade16.test.js
@@ -8,6 +8,6 @@ test('Calcular o produto escalar de dois vetores: [2, 4, 6], [1, 1, 1], expected
   expect(main([2, 4, 6], [1, 1, 1])).toBe(12);
 });
 
-test('Calcular o produto escalar de dois vetores: [1, 2, 3], [1, 2], expected: throwError', () => {
-  expect(main([1, 2, 3], [1, 2])).toThrowError();
+test('Calcular o produto escalar de dois vetores: [1, 2, 3], [1, 2, 3], expected: 14', () => {
+  expect(main([1, 2, 3], [1, 2, 3])).toBe(14);
 });
--- test/atividade31.test.js
+++ test/atividade31.test.js
@@ -4,10 +4,10 @@ test('Verificar se um número é primo de Mersenne: 3, expected: true', () => {
   expect(main(3)).toBe(true);
 });
 
-test('Verificar se um número é primo de Mersenne: 5, expected: true', () => {
-  expect(main(5)).toBe(true);
+test('Verificar se um número é primo de Mersenne: 5, expected: false', () => {
+  expect(main(5)).toBe(false);
 });
 
-test('Verificar se um número é primo de Mersenne: 7, expected: false', () => {
-  expect(main(7)).toBe(false);
+test('Verificar se um número é primo de Mersenne: 7, expected: true', () => {
+  expect(main(7)).toBe(true);
 });
